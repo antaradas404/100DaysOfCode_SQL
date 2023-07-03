60de98eed2d1f767ebd93039a1b72426f31599c4
Antara Das <119549724+antaradas404@users.noreply.github.com>
2023-07-03 00:03:51 -0500

    Delete Day3_LEETCODE_176. Second Highest Salary.sql

--- Day3_LEETCODE_176. Second Highest Salary.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-with temp as
-(select salary, ROW_NUMBER() OVER ( ORDER BY salary desc) as SecondHighestSalary from Employee group by salary )
-#ROW_NUMBER-To not have multiple outputs for 2nd highest
-# group by - To print null if all records have same salary 
-SELECT COALESCE((Select salary from temp where SecondHighestSalary=2), NULL) AS SecondHighestSalary;
-#To print null if query fetches no output
-
-
