3fd554ccdff56c668e10cc33fdc6dfb99031490b
Antara Das <119549724+antaradas404@users.noreply.github.com>
2023-07-03 00:12:55 -0500

    Delete Day6_LEETCODE_1070. Product Sales Analysis III.sql

--- Day6_LEETCODE_1070. Product Sales Analysis III.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-Select product_id , year as first_year , quantity , price 
-from Sales where 
-(product_id, year) in 
-(select product_id, min(year)
-from Sales 
-group by product_id
-order by product_id)
